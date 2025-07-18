9bd753e248e40a0461cb8a2110d7de22d1033c10
gyanvi13 <mehrotragyanvi@gmail.com>
2025-07-18 15:42:48 +0530

    Fix

--- backend/Routes/AuthRoute.js
+++ backend/Routes/AuthRoute.js
@@ -5,8 +5,8 @@ const { Signup, Login } = require('../Controllers/AuthController');
 const { userVerification } = require('../Middlewares/AuthMiddleware'); 
 
 
-router.post('/signup', Signup)
-router.post('/login', Login);
+router.post('/Signup', Signup)
+router.post('/Login', Login);
 router.post('/', userVerification);
 
 
